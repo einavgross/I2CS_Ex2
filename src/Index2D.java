b6aecd9302a6f1ef32e533d21f6b33c87e162a10
einavgross <einav.gross10@gmail.com>
2025-12-19 14:48:26 +0200

    Implementing Index2D class

--- src/Index2D.java
+++ src/Index2D.java
@@ -1,41 +1,48 @@
-package assignments.Ex2;
+//package assignments.Ex2;
 
 public class Index2D implements Pixel2D {
+    private int x;
+    private int y;
     public Index2D(int w, int h) {
-        ;
+        x = w;
+        y = h;
     }
+
     public Index2D(Pixel2D other) {
-        ;
+        x = other.getX();
+        y = other.getY();
     }
+
     @Override
     public int getX() {
-
-        return 0;
+        return x;
     }
 
     @Override
     public int getY() {
-
-        return 0;
+        return y;
     }
 
     @Override
     public double distance2D(Pixel2D p2) {
-
-        return 0;
+        double dx = this.x - p2.getX();
+        double dy = this.y - p2.getY();
+        double pdx = Math.pow(dx, 2);
+        double pdy = Math.pow(dy, 2);
+        return Math.sqrt(pdx + pdy);
     }
 
     @Override
     public String toString() {
-        String ans = null;
-
-        return ans;
+        return "(" + x + "," + y + ")";
     }
 
     @Override
     public boolean equals(Object p) {
         boolean ans = true;
-
+        if (p instanceof Pixel2D p2) {
+            ans = this.x == p2.getX() && this.y == p2.getY();
+        }
         return ans;
     }
 }
--- src/Map.java
+++ src/Map.java
@@ -1,4 +1,3 @@
-package assignments.Ex2;
 import java.io.Serializable;
 /**
  * This class represents a 2D map (int[w][h]) as a "screen" or a raster matrix or maze over integers.
@@ -8,8 +7,6 @@ import java.io.Serializable;
  *
  */
 public class Map implements Map2D, Serializable{
-
-    // edit this class below
 	/**
 	 * Constructs a w*h 2D raster map with an init value v.
 	 * @param w
